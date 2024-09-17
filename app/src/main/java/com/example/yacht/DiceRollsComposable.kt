6cb6dc523f143f00f59301a44263eaf0c64ab12c
KuboyamaNaoki <kuboyama2612@gmail.com>
2024-09-17 18:18:51 +0900

    基本画面の作成（ボタン配置）

--- app/src/main/java/com/example/yacht/DiceRollsComposable.kt
+++ app/src/main/java/com/example/yacht/DiceRollsComposable.kt
@@ -1,2 +1,94 @@
 package com.example.yacht
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+fun DiceRoll() {
+    // Roll the Dices
+    // 5 dice numbers:random pick up (range 1...6)
+}
+
+@Composable
+fun DiceRolls(
+    modifier: Modifier = Modifier
+) {
+    // ダイスの出目をrememberで保持
+//    var diceNumbers = rememberSaveable {
+//        mutableStateListOf(0,0,0,0,0)
+//    }
+
+    Column {
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+                .weight(1f)
+        ) {
+            // さいころの出目を表示
+//            for(num in diceNumbers) {
+//                Text(text = num.toString())
+//            }
+            Text(text = "Text")
+        }
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+                .weight(1f)
+                .align(Alignment.CenterHorizontally),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            RollButton(
+                diceRollsCallback = { /*TODO*/ },
+                modifier = modifier
+                    .padding(16.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun RollButton(
+    diceRollsCallback: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = diceRollsCallback,
+        modifier.padding(16.dp)
+    ) {
+        Text(text = "Roll the Dice!")
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 797,
+    heightDp = 412
+)
+@Composable
+fun PreviewDiceRolls() {
+    DiceRolls()
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+fun PreviewRollButton() {
+    RollButton({})
+}
--- app/src/main/java/com/example/yacht/MainActivity.kt
+++ app/src/main/java/com/example/yacht/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.yacht.ui.theme.YachtTheme
 
@@ -19,21 +27,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             YachtTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppUI(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun AppUI(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back to Home"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            DiceRolls()
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Try to Roll the Dice!",
         modifier = modifier
     )
 }
@@ -42,6 +73,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     YachtTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
